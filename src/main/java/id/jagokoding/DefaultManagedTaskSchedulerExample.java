aecc5c3fb3d675bf929eb6d7ccc1b6b54d3582e2
peri purnama <cisvapery@gmail.com>
2020-10-01 18:21:53 +0800

    Initial commit

---
+++ src/main/java/id/jagokoding/DefaultManagedTaskSchedulerExample.java
@@ -0,0 +1,69 @@
+package id.jagokoding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+
+import javax.naming.NamingException;
+import java.time.LocalTime;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DefaultManagedTaskSchedulerExample {
+
+    public static void main (String[] args) throws NamingException, InterruptedException {
+    	// mengikat penjadwal secara manual,
+        // Dalam lingkungan server yang mendukung JEE, ini akan terjadi
+        // disediakan oleh produk server
+
+        SimpleNamingContextBuilder b = new SimpleNamingContextBuilder();
+        b.bind("java:comp/DefaultManagedScheduledExecutorService",
+               Executors.newScheduledThreadPool(5));
+        b.activate();
+
+        //bootstrapping spring
+        ApplicationContext context =
+                  new AnnotationConfigApplicationContext(MyConfig.class);
+        MyBean bean = context.getBean(MyBean.class);
+        bean.runTask();
+        //shutdown after 10 sec
+        Thread.sleep(10000);
+        DefaultManagedTaskScheduler scheduler = context.getBean(
+                  DefaultManagedTaskScheduler.class);
+        Executor exec = scheduler.getConcurrentExecutor();
+        ExecutorService es = (ExecutorService) exec;
+        System.out.println("shutting down executor service");
+        es.shutdownNow();
+    }
+
+    @Configuration
+    public static class MyConfig {
+
+        @Bean
+        TaskScheduler taskScheduler () {
+            return new DefaultManagedTaskScheduler();
+        }
+
+        @Bean
+        MyBean myBean () {
+            return new MyBean();
+        }
+    }
+
+    private static class MyBean {
+        @Autowired
+        TaskScheduler taskScheduler;
+
+        public void runTask () {
+            taskScheduler.scheduleWithFixedDelay((Runnable) () -> {
+                System.out.println("running " + LocalTime.now());
+            }, 1000L);
+        }
+    }
+}
